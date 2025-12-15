2f0c8c0fe970396e55e7618df6e8cc3240d8809d
merabetfatima2004-ui <merabetfatima2004@gmail.com>
2025-12-15 11:39:38 +0100

    ADN_Sequence.py

--- ADN_Sequence.py
+++ ADN_Sequence.py
@@ -1,7 +1,6 @@
-#Master 1 Génétique|Merabet fatima|Lien Githup :
-http://githup.com /Merabet fatima/ Analyse-Séquece
--ADN | Date : 15/12/2025
-#Membre de groupe : Merabet fatima 
+# Master 1 Génétique | Chef du projet : Chebab Nesrine | Lien GitHub : https://github.com/merabetfatima2004-ui/Analyse-Sequence-ADN | Date : 15/12/2025
+# Membres du groupe : Merabet Chaima
+
 import pandas as pd
 
 # 1) Création du tableau de données
@@ -15,8 +14,8 @@ data = {
         "CGTACGTAGC",
         "TTAACCGGAT"
     ],
-    "Longueur": [12, 12, 12, 10, 11, 10, 10],
-    "Pourcentage GC": [50, 66.67, 58.33, 40, 45.45, 60, 50]
+    "Longueur": [11, 12, 12, 10, 11, 10, 10],
+    "Pourcentage GC": [45.45, 66.67, 58.33, 40, 45.45, 60, 50]
 }
 
 df = pd.DataFrame(data)
@@ -24,20 +23,20 @@ df = pd.DataFrame(data)
 print("Tableau initial :")
 print(df)
 
-# 2) Sélection et affichage de la colonne "Longueur"
+# 2) Sélection de la colonne Longueur
 print("\nColonne Longueur :")
 print(df["Longueur"])
 
-# 3) Filtrage des séquences dont la longueur est supérieure à 10
+# 3) Filtrage longueur > 10
 df_filtre = df[df["Longueur"] > 10]
 print("\nSéquences dont la longueur > 10 :")
 print(df_filtre)
 
-# 4) Calcul du pourcentage moyen de GC (3 chiffres après la virgule)
+# 4) Moyenne GC
 moyenne_gc = round(df["Pourcentage GC"].mean(), 3)
 print("\nPourcentage moyen de GC :", moyenne_gc)
 
-# 5) Ajout de la colonne "Catégorie GC"
+# 5) Catégorie GC
 def categorie_gc(gc):
     if gc > 55:
         return "Riche"
@@ -48,17 +47,13 @@ def categorie_gc(gc):
 
 df["Catégorie GC"] = df["Pourcentage GC"].apply(categorie_gc)
 
-# 6) Ajout d’une colonne indiquant le nombre de 'G' dans chaque séquence
+# 6) Nombre de G
 df["Nombre de G"] = df["Séquence"].apply(lambda x: x.count("G"))
 
-# 7) Calcul de l’écart-type du %GC et de la longueur
-ecart_type_gc = df["Pourcentage GC"].std()
-ecart_type_longueur = df["Longueur"].std()
+# 7) Écart-type
+print("\nÉcart-type GC :", round(df["Pourcentage GC"].std(), 3))
+print("Écart-type Longueur :", round(df["Longueur"].std(), 3))
 
-print("\nÉcart-type du %GC :", round(ecart_type_gc, 3))
-print("Écart-type de la longueur :", round(ecart_type_longueur, 3))
-
-# 8) Sauvegarde du tableau final dans un fichier CSV
+# 8) Sauvegarde CSV
 df.to_csv("analyse_sequences_ADN.csv", index=False)
-
-print("\nLe tableau final a été sauvegardé dans le fichier 'analyse_sequences_ADN.csv'")
+print("\nFichier CSV sauvegardé avec succès")
